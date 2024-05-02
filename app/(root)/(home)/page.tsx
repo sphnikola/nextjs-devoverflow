efc6f7cd05b652cc75349fe86923b6576d957d00
sphnikola <sphnikola@gmail.com>
2024-05-02 02:24:32 +0100

    Add authentication

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <main>
       <h1>Home Page</h1>
+      <UserButton afterSignOutUrl="/" />
     </main>
   );
 }
